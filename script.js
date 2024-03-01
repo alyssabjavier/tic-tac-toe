9edd453de3611891e792095e11228d4dd63a018d
“alyssabjavier” <alyssabjavier@gmail.com>
2024-02-29 20:52:17 -0600

    Fixed check win function

--- script.js
+++ script.js
@@ -24,24 +24,35 @@ const getNewPlayer = (function() {
 })();
 
 //gameController
+
 //each player picks a tile aka chooses an empty grid cell to set to their marker
-function markCell(player, row, column) {
+
+function markCell(row, column) {
     if (Gameboard.gameboard[row][column] === null) {
-        Gameboard.gameboard[row][column] = player.marker;
-        return Gameboard.showBoard();
+        Gameboard.gameboard[row][column] = activePlayer.marker;
+        return true;
     } else {
         return false;
     }
 };
 
+function playRound(row, column) {
+    markCell(row, column);
+    switchPlayerTurn();
+    checkBoard();
+    return Gameboard.showBoard();
+}
+
 //switch turns
 function switchPlayerTurn() {
-    let activePlayer = playerOne;
+    // let activePlayer = playerOne;
     if (activePlayer === playerOne) {
         activePlayer = playerTwo;
+        return activePlayer;
     }
     if (activePlayer === playerTwo) {
         activePlayer = playerOne;
+        return activePlayer;
     }
 }
 
@@ -80,39 +91,44 @@ function checkWin() {
         [[0, 0], [1, 1], [2, 2]], // top-left to bottom-right diagonal
         [[0, 2], [1, 1], [2, 0]]  // top-right to bottom-left diagonal
     ];
+    
+    let winner = null;
 
     winConditions.forEach(condition => {
         let [[a,b], [c,d], [e,f]] = condition;
 
         //if all 3 cells have the same marker
-        if (Gameboard.gameboard[a][b] === Gameboard.gameboard[c][d] && Gameboard.gameboard[a][b] === Gameboard.gameboard[e][f]) {
-            if (Gameboard.gameboard[a][b] === playerOne.marker) {
-                winner = playerOne;
-            };
-            if (Gameboard.gameboard[a][b] === playerTwo.marker) {
-                winner = playerTwo;
-            };
+        if (Gameboard.gameboard[a][b] === playerOne.marker && Gameboard.gameboard[c][d] === playerOne.marker && Gameboard.gameboard[e][f] === playerOne.marker) {
+            winner = playerOne;
+        }
+        if (Gameboard.gameboard[a][b] === playerTwo.marker && Gameboard.gameboard[c][d] === playerTwo.marker && Gameboard.gameboard[e][f] === playerTwo.marker) {
+            winner = playerTwo;
         }
     });
-    return `${winner.name} wins`;
+    if (!winner===null) {
+        return `${winner.name} wins`;
+    } else {
+        return false;
+    }
 }
 
 //for testing
 
 let playerOne = getNewPlayer('alyssa', 'X');
 console.log(playerOne);
+let activePlayer = playerOne;
 
 let playerTwo = getNewPlayer('yoshi', 'O');
 console.log(playerTwo);
 
-markCell(playerTwo, 0, 0);
-markCell(playerTwo, 0, 1);
-markCell(playerTwo, 0, 2);
+// markCell(playerTwo, 0, 0);
+// markCell(playerTwo, 0, 1);
+// markCell(playerTwo, 0, 2);
 
-markCell(playerOne, 1, 0);
-markCell(playerOne, 1, 1);
-markCell(playerOne, 1, 2);
+// markCell(playerOne, 1, 0);
+// markCell(playerOne, 1, 1);
+// markCell(playerOne, 1, 2);
 
-markCell(playerTwo, 2, 0);
-markCell(playerTwo, 2, 1);
-markCell(playerTwo, 2, 2);
+// markCell(playerTwo, 2, 0);
+// markCell(playerTwo, 2, 1);
+// markCell(playerTwo, 2, 2);
